73359969d9c19e089103972ebf5a92f1f1ae2762
Alex Palermo <alex.palermo.45@gmail.com>
2024-08-22 18:26:28 -0400

    cleaned up day 1, C

---
+++ 2023/day1/C/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  char a = 'a';
+  printf("as a character: %c\n", a);
+  printf("as a number: %d\n", a);
+  printf("character 0 is: %d\n", '0');
+  printf("character 2 is: %d\n", '2');
+  return 0;
+}
