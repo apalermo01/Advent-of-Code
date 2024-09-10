5f098721137c9a037875ab80741a89d34b9d070c
Alex Palermo <alex.palermo.45@gmail.com>
2024-09-10 08:07:38 -0400

    solved day 3 part 1

--- 2023/day3/python/main.py
+++ 2023/day3/python/main.py
@@ -16,8 +16,8 @@ def generate_matches(line):
 
     matches = []
     s = True
-    index = 0
-
+    curr_idx = 0
+    orig_line = line
     while s is not None:
 
         s = re.search(".?\d+.?", line)
@@ -26,22 +26,15 @@ def generate_matches(line):
             matches.append(
                 {'match': this_match,
                  'value': int(re.findall("\d+", this_match)[0]),
-                 'start_idx': s.start() + index,
-                 'end_idx': s.end() + index - 1,
+                 'start_idx': s.start() + curr_idx,
+                 'end_idx': s.end() + curr_idx,
                  })
-            old_line = line
 
-            if this_match[-1].isnumeric():
-                forward = len(this_match)
-            else:
-                forward = len(this_match)-1
-
-            index = line.index(this_match) + forward
-
-            line = line[index:]
-            if line == old_line:
-                raise ValueError("bad iteration!")
-    print("matches = ", matches)
+            # now move the line forward past the found value
+            curr_idx += s.end()
+            if orig_line[curr_idx].isdigit():
+                curr_idx -= 1
+            line = orig_line[curr_idx:]
     return matches
 
 
@@ -49,20 +42,20 @@ def symbol_nearby(match: Dict,
                   line_below: str,
                   this_line: str,
                   line_above: str):
-    print("+++++++++++++++++++++")
-    print("match = ", match)
-    print("line below = ", line_below)
-    print("this line = ", this_line)
-    print("line above = ", line_above)
-
-    for line in [line_below, this_line, line_above]:
-        for char in range(match['start_idx'], match['end_idx']):
+    line_below_chunk = line_below[
+        match['start_idx']:match['end_idx']
+    ]
+    this_line_chunk = this_line[
+        match['start_idx']:match['end_idx']
+    ]
+    line_above_chunk = line_above[
+        match['start_idx']:match['end_idx']
+    ]
+    for line in [line_below_chunk, this_line_chunk, line_above_chunk]:
+        for char in range(len(line)):
             if (not line[char].isdigit() and line[char] != '.'):
-                print("adding ths part number to sum")
                 return True
 
-    print("not adding this part number to sum")
-    print("+++++++++++++++++++++")
     return False
 
 
@@ -73,7 +66,6 @@ def get_part_nums(line_below, this_line, line_above):
     matches = generate_matches(this_line)
 
     for match in matches:
-
         if (symbol_nearby(match, line_below, this_line, line_above)):
             part_nums.append(match['value'])
 
@@ -85,7 +77,6 @@ def problem_1(lines: List[str]) -> int:
     part_nums = []
 
     for row_idx in range(len(lines)):
-
         # grab this line and the one above / below
         this_line = lines[row_idx]
         if row_idx == 0:
@@ -102,38 +93,6 @@ def problem_1(lines: List[str]) -> int:
         part_nums.append(get_part_nums(line_below, this_line, line_above))
 
     return sum(part_nums)
-
-
-# def problem_1(lines):
-#
-#     LEN_LINES = len(lines)
-#
-#     part_nums = []
-#     count = min(len(lines), 5)
-#     for row_idx in range(len(lines)):
-#         line = lines[row_idx]
-#         match_strings = generate_matches(line)
-#         nums_this_line = []
-#         for match in match_strings:
-#             surrounding_chars = match
-#             index_start = line.index(match)
-#
-#             if row_idx > 0:
-#                 line_above = lines[row_idx -
-#                                    1][index_start:index_start+len(match)]
-#                 surrounding_chars += line_above
-#
-#             if row_idx < LEN_LINES-1:
-#                 line_below = lines[row_idx +
-#                                    1][index_start:index_start+len(match)]
-#                 surrounding_chars += line_below
-#
-#             mask = [not (c.isnumeric() or c == '.' or c == '\n')
-#                     for c in surrounding_chars]
-#             if any(mask):
-#                 nums_this_line.append(int(re.findall("\d+", match)[0]))
-#         part_nums += nums_this_line
-#     return sum(part_nums)
 
 
 def problem_2(lines):
