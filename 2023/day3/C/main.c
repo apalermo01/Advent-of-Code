fbdc5cd60b6b765655fb81ef9b968c0280b505d6
Alex Palermo <alex.palermo.45@gmail.com>
2024-09-19 08:54:42 -0400

    implemented forward and backward search and cases for finding part numbers

--- 2023/day3/C/main.c
+++ 2023/day3/C/main.c
@@ -1,7 +1,10 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUF_SIZE 65536
+#define MAX_DIGITS 4
 
 // define a structure for returning a tuple that represents the information we
 // need to grab from the file before doing anything else
@@ -10,6 +13,35 @@ struct line_info {
   int max_width;
 };
 
+/* define a structure that represents a 2d array. The objective here is to make
+ * indexing as easy as possible.
+ *
+ * The array will actually be stored as a 1-dimensional array. The 2d aspect
+ * will all come from the indexing logic.
+ */
+struct array2d {
+  char *arr;
+  int numrows;
+  int numcols;
+};
+
+int string_to_num(char *str) {
+  int total = 0;
+  int pow = 1;
+
+  int num_digits = strnlen(str, 8);
+  int num;
+
+  for (int i = num_digits; i >= 0; i--) {
+    if (str[i] == '\0')
+      continue;
+    num = str[i] - '0';
+    total += pow * num;
+    pow *= 10;
+  }
+
+  return total;
+}
 /*
  * adapted from: https://stackoverflow.com/a/70708991
  * Counts the number of lines in a file and the maximum
@@ -56,18 +88,6 @@ struct line_info count_lines(FILE *file) {
   return ret;
 }
 
-/* define a structure that represents a 2d array. The objective here is to make
- * indexing as easy as possible.
- *
- * The array will actually be stored as a 1-dimensional array. The 2d aspect
- * will all come from the indexing logic.
- */
-struct array2d {
-  char *arr;
-  int numrows;
-  int numcols;
-};
-
 /* initialization logic for array */
 struct array2d make_array(int nrows, int ncols) {
   char *array = malloc(sizeof(char) * nrows * ncols);
@@ -103,10 +123,7 @@ int get_value(struct array2d arr, int row, int col) {
   return arr.arr[arr.numcols * col + row];
 }
 
-void freearr(struct array2d *arr) {
-  free(arr->arr);
-  free(arr);
-}
+void freearr(struct array2d *arr) { free(arr->arr); }
 
 /*
  * Take a file and convert it to a 2d array
@@ -123,9 +140,7 @@ struct array2d file_to_array(FILE *file, struct line_info linfo) {
   struct array2d arr = make_array(nrows, ncols);
 
   while ((read = getline(&line, &len, file)) != -1) {
-    /*printf("line = %s\n", line);*/
     for (thiscol = 0; thiscol < ncols; thiscol++) {
-      /*printf("setting (%d, %d) to %c\n", thisrow, thiscol, line[thiscol]);*/
       set_in_array(arr, thisrow, thiscol, line[thiscol]);
     }
     thisrow++;
@@ -136,10 +151,20 @@ struct array2d file_to_array(FILE *file, struct line_info linfo) {
 
 void print_row(struct array2d array, int row) {
   if (row >= array.numrows) {
-    fprintf(stderr, "ERROR: row %d is out of bounds for array with %d rows\n", row, array.numrows);
+    fprintf(stderr, "ERROR: row %d is out of bounds for array with %d rows\n",
+            row, array.numrows);
   }
   for (int j = 0; j < array.numcols; j++) {
     printf("%c", get_value(array, row, j));
+  }
+}
+
+void get_row(struct array2d array, int row, char *buf, size_t buffsize) {
+  if (buffsize > array.numcols) {
+    fprintf(stderr, "get_row: passed buffer is too small for array's row size");
+  }
+  for (int j = 0; j < array.numcols; j++) {
+    buf[j] = get_value(array, row, j);
   }
 }
 
@@ -168,15 +193,14 @@ struct array2d pad_array(struct array2d arr) {
   }
 
   // left and right columns
-  for (int i = 0; i < nrows-1; i++) {
+  for (int i = 0; i < nrows - 1; i++) {
 
     if (set_in_array(new_array, i, 0, '.') != 0) {
       fprintf(stderr, "error assigning value: row=%d, col=%d\n", i, 0);
     }
-    if (set_in_array(new_array, i, ncols-1, '.') != 0) {
+    if (set_in_array(new_array, i, ncols - 1, '.') != 0) {
       fprintf(stderr, "error assigning value: row=%d, col=%d\n", i, ncols);
     }
-
   }
 
   return new_array;
@@ -187,14 +211,92 @@ struct array2d pad_array(struct array2d arr) {
  * iterate backwards through the buffer until we hit a non-number,
  * then concatenate the resulting characters to a number and return it.
  */
-int backward_search(char *buf, int starting_pos) { return 0; }
+int backward_search(char *buf, int starting_pos, int max_digits,
+                    size_t buffsize) {
+
+  printf("running backward search\n");
+  char *found_digits_tmp = malloc(max_digits * sizeof(char));
+  int num_digits_found = 0;
+
+  if (found_digits_tmp == NULL) {
+    fprintf(stderr, "Error allocating memory for backward search");
+    return 0;
+  }
+
+  for (int offset = 0; offset < max_digits; offset++) {
+    if (isdigit(buf[starting_pos - offset]) == 0 || starting_pos - offset < 0) {
+      break;
+    }
+    found_digits_tmp[max_digits - offset - 1] = buf[starting_pos - offset];
+    num_digits_found++;
+  }
+
+  char *found_digits = malloc((num_digits_found + 1) * sizeof(char));
+  if (found_digits == NULL) {
+    free(found_digits_tmp);
+    fprintf(stderr, "Error allocating memory for backward search");
+    return 0;
+  }
+
+  for (int i = 0; i < num_digits_found; i++) {
+    found_digits[i] = found_digits_tmp[max_digits - num_digits_found + i];
+  }
+  found_digits[num_digits_found] = '\0';
+
+  int ret = string_to_num(found_digits);
+
+  free(found_digits_tmp);
+  free(found_digits);
+
+  printf("ret is %d\n", ret);
+  return ret;
+}
 
 /*
  * Given some buffer and starting at the position indicated by starting_pos,
  * iterate forwards through the buffer until we hit a non-number,
  * then concatenate the resulting characters to a number and return it.
  */
-int forward_search(char *buf, int starting_pos) { return 0; }
+int forward_search(char *buf, int starting_pos, int max_digits,
+                   size_t buffsize) {
+  printf("running forward search\n");
+  char *found_digits_tmp = malloc(max_digits * sizeof(char));
+  int num_digits_found = 0;
+
+  if (found_digits_tmp == NULL) {
+    fprintf(stderr, "Error allocating memory for forward search");
+    return 0;
+  }
+
+  for (int offset = 0; offset < max_digits; offset++) {
+    if (isdigit(buf[starting_pos + offset]) == 0 ||
+        starting_pos + offset - 1 > buffsize) {
+      break;
+    }
+    found_digits_tmp[offset] = buf[starting_pos + offset];
+    num_digits_found++;
+  }
+
+  char *found_digits = malloc((num_digits_found + 1) * sizeof(char));
+  if (found_digits == NULL) {
+    free(found_digits_tmp);
+    fprintf(stderr, "Error allocating memory for forward search");
+    return 0;
+  }
+
+  for (int i = 0; i < num_digits_found; i++) {
+    found_digits[i] = found_digits_tmp[i];
+  }
+  found_digits[num_digits_found] = '\0';
+
+  int ret = string_to_num(found_digits);
+
+  free(found_digits);
+  free(found_digits_tmp);
+
+  printf("ret is %d\n", ret);
+  return ret;
+}
 
 /*
  * Given some buffer and starting at the position indicated by starting_pos,
@@ -202,7 +304,50 @@ int forward_search(char *buf, int starting_pos) { return 0; }
  * backwards until we hit a non-number, then concatenate the resulting
  * characters to a number and return it.
  */
-int middle_search(char *buf, int starting_pos) { return 0; }
+int middle_search(char *buf, int starting_pos, int max_digits,
+                  size_t buffsize) {
+  printf("running middle search\n");
+  char *found_digits_tmp = malloc((max_digits * 2 + 1) * sizeof(char));
+  int num_digits_found = 0;
+
+  if (found_digits_tmp == NULL) {
+    fprintf(stderr, "Error allocating memory for forward search");
+    return 0;
+  }
+
+  // forward 
+  int offset;
+  for (offset = 0; offset < max_digits; offset ++) {
+    if (isdigit(buf[starting_pos+offset]) == 0 || starting_pos + offset - 1 > buffsize) {
+      break;
+    }
+
+    found_digits_tmp[max_digits+offset] = buf[starting_pos + offset];
+    num_digits_found ++;
+  }
+
+  // backward
+  for (offset = -1; offset > max_digits * -1; offset --) {
+    if (isdigit(buf[starting_pos + offset]) == 0 || starting_pos + offset < 0) {
+      break;
+    }
+
+    found_digits_tmp[max_digits+offset] = buf[starting_pos + offset];
+    num_digits_found ++;
+  }
+
+  char *found_digits = malloc((num_digits_found+1) * sizeof(char));
+  if (found_digits == NULL) {
+    free(found_digits_tmp);
+    fprintf(stderr, "Error allocating memory for middle search");
+    return 0;
+  }
+  
+  /*for (int i = 0; i < num_digits_found; i++) {*/
+  /*  found_digits[i] = found_digits_tmp[]*/
+  /*}*/
+  return 0;
+}
 /*
  * core logic for problem 1
  * Objective: the 2d array has a collection of numbers and symbols
@@ -211,21 +356,237 @@ int middle_search(char *buf, int starting_pos) { return 0; }
  * add up all the part numbers in the array
  */
 
-int problem_1(struct array2d text) {
+int get_part_numbers(struct array2d text, int row, int col) {
+  /*
+   * We're searching around the text array like this:
+   *
+   * ? ? ?
+   * ? * ?
+   * ? ? ?
+   *
+   * where * is located at (row, col) in text and we need to check if ?
+   * is a number. If ? is a number, we need to recover and concatenate together
+   * the other numbers. Here I'm going to list out all the cases we need to
+   * check for. The top and bottom row have similar logic so I'll count those as
+   * the same cases.
+   *
+   * 1:
+   * # . .
+   * ? * ?
+   *
+   * 2:
+   * . # .
+   * ? * ?
+   *
+   * 3:
+   * . . #
+   * ? * ?
+   *
+   * 4:
+   * # # .
+   * ? * ?
+   *
+   * 5:
+   * . # #
+   * ? * ?
+   *
+   * 6:
+   * # . #
+   * ? * ?
+   *
+   * 7:
+   * ? ? ?
+   * # * #
+   */
+  int part_total = 0;
 
-  for (int i = 0; i < text.numrows; i++) {
-    for (int j = 0; j < text.numcols; j++) {
-    }
+  // create an array of bools that describe which case(s) we're dealing with
+  int top_cases[] = {isdigit(get_value(text, row - 1, col - 1)),
+                     isdigit(get_value(text, row - 1, col)),
+                     isdigit(get_value(text, row - 1, col + 1))};
+  int middle_cases[] = {isdigit(get_value(text, row, col - 1)),
+                        isdigit(get_value(text, row, col + 1))};
+  int bottom_cases[] = {isdigit(get_value(text, row + 1, col - 1)),
+                        isdigit(get_value(text, row + 1, col)),
+                        isdigit(get_value(text, row + 1, col + 1))};
+
+  char *top_line = malloc(text.numcols * sizeof(char));
+  char *middle_line = malloc(text.numcols * sizeof(char));
+  char *bottom_line = malloc(text.numcols * sizeof(char));
+
+  get_row(text, row - 1, top_line, text.numcols);
+  get_row(text, row, middle_line, text.numcols);
+  get_row(text, row + 1, bottom_line, text.numcols);
+
+  if (top_line == NULL || middle_line == NULL || bottom_line == NULL) {
+    free(top_line);
+    free(middle_line);
+    free(bottom_line);
+    fprintf(stderr, "Error allocating memory for lines, returning 0");
+    return 0;
   }
+
+  printf("=============\n");
+  printf("top line is:    %s\n", top_line);
+  printf("middle line is: %s\n", middle_line);
+  printf("bottom line is: %s\n", bottom_line);
+  printf("coords of * are: (%d, %d)\n", row, col);
+
+  /*
+   * case 1:
+   *
+   * # . .     ? ? ?
+   * ? * ? or  ? * ?
+   * ? ? ?     # . .
+   */
+  if (top_cases[0] > 0 && top_cases[1] == 0 && top_cases[2] == 0) {
+    printf("evaluating case 1 for top\n");
+    part_total += backward_search(top_line, col - 1, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] > 0 && bottom_cases[1] == 0 && bottom_cases[2] == 0) {
+    printf("evaluating case 1 for bottom\n");
+    part_total +=
+        backward_search(bottom_line, col - 1, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 2:
+   *
+   * . # .    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    . # .
+   */
+  if (top_cases[0] == 0 && top_cases[1] > 0 && top_cases[2] == 0) {
+    printf("evaluating case 2 for top\n");
+    part_total += top_line[col] - '0';
+  }
+  if (bottom_cases[0] == 0 && bottom_cases[1] > 0 && bottom_cases[2] == 0) {
+    printf("evaluating case 2 for bottom\n");
+    part_total += bottom_line[col] - '0';
+  }
+
+  /*
+   * case 3:
+   *
+   * . . #    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    . . #
+   */
+  if (top_cases[0] == 0 && top_cases[1] == 0 && top_cases[2] > 0) {
+    printf("evaluating case 3 for top\n");
+    part_total += forward_search(top_line, col + 1, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] == 0 && bottom_cases[1] == 0 && bottom_cases[2] > 0) {
+    printf("evaluating case 3 for bottom\n");
+    part_total += forward_search(top_line, col + 1, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 4:
+   *
+   * # # .    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    # # .
+   */
+  if (top_cases[0] > 0 && top_cases[1] > 0 && top_cases[2] == 0) {
+    printf("evaluating case 4 for top\n");
+    part_total += backward_search(top_line, col, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] > 0 && bottom_cases[1] > 0 && bottom_cases[2] == 0) {
+    printf("evaluating case 4 for bottom\n");
+    part_total += backward_search(bottom_line, col, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 5:
+   *
+   * . # #    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    . # #
+   */
+  if (top_cases[0] == 0 && top_cases[1] > 0 && top_cases[2] > 0) {
+    printf("evaluating case 5 for top\n");
+    part_total += forward_search(top_line, col, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] == 0 && bottom_cases[1] > 0 && bottom_cases[2] > 0) {
+    printf("evaluating case 5 for bottom\n");
+    part_total += forward_search(bottom_line, col, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 6:
+   *
+   * # . #    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    # . #
+   */
+  if (top_cases[0] > 0 && top_cases[1] == 0 && top_cases[2] > 0) {
+    printf("evaluating case 6 top\n");
+    part_total += backward_search(top_line, col - 1, MAX_DIGITS, text.numcols);
+    part_total += forward_search(top_line, col + 1, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] > 0 && bottom_cases[1] == 0 && bottom_cases[2] > 0) {
+    printf("evaluating case 6 bottom\n");
+    part_total +=
+        backward_search(bottom_line, col - 1, MAX_DIGITS, text.numcols);
+    part_total +=
+        forward_search(bottom_line, col + 1, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 7:
+   *
+   * # # #    ? ? ?
+   * ? * ? or ? * ?
+   * ? ? ?    # # #
+   */
+  if (top_cases[0] > 0 && top_cases[1] > 0 && top_cases[2] > 0) {
+    printf("evaluating case 7 for top\n");
+    part_total += middle_search(top_line, col, MAX_DIGITS, text.numcols);
+  }
+  if (bottom_cases[0] > 0 && bottom_cases[1] > 0 && bottom_cases[2] > 0) {
+    printf("evaluating case 7 for bottom\n");
+    part_total += middle_search(bottom_line, col, MAX_DIGITS, text.numcols);
+  }
+
+  /*
+   * case 8:
+   *
+   * middle line has numbers
+   */
+
+  if (middle_cases[0] > 0) {
+    printf("evaluating case 8.backwards for middle\n");
+    part_total +=
+        backward_search(middle_line, col - 1, MAX_DIGITS, text.numcols);
+  }
+  if (middle_cases[1] > 0) {
+    printf("evaluating case 8.forwards for middle\n");
+    part_total +=
+        forward_search(middle_line, col + 1, MAX_DIGITS, text.numcols);
+  }
+
+  free(top_line);
+  free(middle_line);
+  free(bottom_line);
+  return part_total;
+}
+
+int problem_1(struct array2d text) {
+  int part_total = 0;
 
   // pad the array so we can treat everything equally
   text = pad_array(text);
 
-  for (int i = 0; i < text.numrows; i++) {
-    print_row(text, i);
-    printf("\n");
+  // loop through the "core" of the file
+  for (int i = 1; i < text.numrows - 1; i++) {
+    for (int j = 1; j < text.numcols - 1; j++) {
+      if (get_value(text, i, j) == '*') {
+        part_total += get_part_numbers(text, i, j);
+      }
+    }
   }
-  return 0;
+  return part_total;
 }
 
 // test the 2d array functionality
